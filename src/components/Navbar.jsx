b950124ae4098b644d988a30e04d7d2dfd7ae1ac
muhammadsyauqijazuli <syauqialjm93@gmail.com>
2025-09-17 11:24:36 +0700

    modification navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect} from "react";
 
 const Navbar = () => {
     const [active, setActive] = useState(false);
@@ -13,24 +13,34 @@ const Navbar = () => {
         };
 
         window.addEventListener("scroll", handleScroll);
-
         return () => {
             window.removeEventListener("scroll", handleScroll);
         };
     }, []);
 
     return (
-        <div className="navbar py-7 flex items-center justify-between">
+        <div className="navbar py-7 flex justify-between items-center">
             <div className="logo">
-                <h1 className="text-3xl font-bold bg-white text-black p-1
+                <h1 className="text-3xl font-bold bg-white text-black p-1*
                 md:bg-transparent md:text-white">Portofolio</h1>
             </div>
-            <ul className={`menu flex items-center gap-10 md:static fixed left-1/2 translate-x-1/2 
-            md:-translate-x-0 md:opacity-100 md:top-0 ${active ? "top-0 opacity-100" : "-top-10 opacity-0"}`}>
-                <li><a className="text-lg font-medium" href="">Beranda</a></li>
-                <li><a className="text-lg font-medium" href="">Tentang</a></li>
-                <li><a className="text-lg font-medium" href="">Proyek</a></li>
-                <li><a className="text-lg font-medium" href="">Kontak</a></li>
+            <ul className={`menu flex items-center sm:gap-10 gap-4 md:static fixed left-1/2 -translate-x-1/2
+            md:-translate-0 md:opacity-100 bg-white/30 backdrop-blur-md 
+            p-4 rounded-br-2xl rounded-bl-2xl md:bg-transparent transition-all md:transform-none ${
+                    active ? "top-0 opacity-100" : "-top-10 opacity-0"
+                }`}>
+                <li>
+                    <a href="#" className="sm:text-lg text-base font-medium">Beranda</a>
+                </li>
+                <li>
+                    <a href="#" className="sm:text-lg text-base font-medium">Tentang</a>
+                </li>
+                <li>
+                    <a href="#" className="sm:text-lg text-base font-medium">Proyek</a>
+                </li>
+                <li>
+                    <a href="#" className="sm:text-lg text-base font-medium">Kontak</a>
+                </li>
             </ul>
         </div>
     )
