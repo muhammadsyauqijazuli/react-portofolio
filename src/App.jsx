c10b24aa58f72bb62ae055f3c2279a154f5899b2
muhammadsyauqijazuli <syauqialjm93@gmail.com>
2025-09-18 12:36:09 +0700

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import DataImage from "./data";
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo ipsum earum perspiciatis at provident repellendus, ratione ex id dolore mollitia repellat maxime corporis asperiores alias iste. Officia repudiandae dolorum nesciunt alias similique quos sunt voluptatibus doloremque porro molestias veritatis temporibus possimus minima ipsum exercitationem, ducimus eius repellat inventore id odio neque reiciendis quibusdam? Sed similique minus saepe illo sunt earum nemo provident maxime non nihil! Nemo obcaecati impedit ad neque illum, error necessitatibus. Quas dolorem ut aliquid vel magni ab sunt necessitatibus voluptatibus accusamus saepe minima aut adipisci, soluta fuga quo maxime officia, deleniti, molestiae laborum. Laboriosam quae iure eos, nobis impedit sequi ab. Inventore delectus harum obcaecati, aut eveniet voluptates non, in possimus nesciunt neque facere consequatur aspernatur? Consectetur, eum temporibus? Cumque veniam itaque nostrum aut aperiam, deserunt quod, deleniti ea suscipit illum porro iure nisi culpa. Ipsa, sed. Eaque ullam assumenda quasi explicabo perferendis, necessitatibus et sapiente quis! Voluptatum quibusdam nihil placeat, voluptatem doloremque consequatur autem sint minus nulla cumque reprehenderit blanditiis sit expedita doloribus, maxime quasi odio. Qui quasi suscipit neque placeat repellendus soluta eos illum ex atque natus obcaecati fugit, in ab nobis magnam eius facere explicabo at? Distinctio cupiditate dignissimos aut saepe voluptatum consequatur eos?
-      </h1>
+      <div className="hero grid md:grid-cols-2 pt-10 items-center xl:gap-0 gap-6 grid-cols-1">
+        <div>
+          <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
+            <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md" />
+            <q>kode yang indah, lahir dari ketekunan. 😁</q>
+          </div>
+          <h1 className="text-5xl/tight font-bold mb-6">Hi, Saya Syauqi Jazuli</h1>
+          <p className="text-base/loose mb-6 opacity-50">
+            I am a student at Universitas Malikussaleh in Lhokseumawe, passionate about Information
+            Technology. My expertise spans cloud computing and web development, reflecting my dedication 
+            to development and creativity. My diverse skills are my strength in creating comprehensive 
+            and high-quality technology solutions.
+          </p>
+          <div className="flex items-center sm:gap-4 gap-2">
+            <a href="#" className="bg-violet-700 p-4 rounded-2xl hover:bg-violet-600">
+              Download CV <i className="ri-download-line ri-lg"></i>
+            </a>
+            <a href="#" className="bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-600">
+              Lihat Proyek 
+              <i className="ri-arrow-down-line ri-lg"></i>
+            </a>
+          </div>
+        </div>
+        <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto" />
+      </div>
     </>
   )
 }
