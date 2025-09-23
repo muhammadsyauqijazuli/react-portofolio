b653a065acf49f31258194ec4aea4ede79b28dd2
muhammadsyauqijazuli <syauqialjm93@gmail.com>
2025-09-23 13:54:40 +0700

    add contact section

--- src/App.jsx
+++ src/App.jsx
@@ -105,7 +105,7 @@ function App() {
                     ))}
                   </div>
                   <div className="mt-8 text-center">
-                    <a href="#" className="bg-violet-700 p3 rounded-lg block border
+                    <a href="#" className="bg-violet-700 p-3 rounded-lg block border
                     border-zinc-600 hover:bg-violet-600">Lihat Website</a>
                   </div>
                 </div>
@@ -114,6 +114,37 @@ function App() {
         </div>  
       </div>
       {/* Proyek */}
+
+      {/* Kontak */}
+      <div className="kontak mt-32 p-10">
+        <h1 className="text-4xl mb-2 font-bold text-center">Kontak</h1>
+        <p className=" text-base/loose text-center mb-10 opacity-50">Mari Terhubung dengan saya</p>
+        <form action="https://formsubmit.co/syauqialjm93@gmail.com" method="POST" className="bg-zinc-800 p-10 w-fit mx-auto rounded-md" 
+        autoComplete="off">
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+              <label className="font-semibold">Nama Lengkap</label>
+              <input type="text" name="nama" placeholder="Masukkan Nama..." className="border
+              border-zinc-500 p-2 rounded-md" required/>
+            </div>
+            <div className="flex flex-col gap-2">
+              <label className="font-semibold">Email</label>
+              <input type="Email" name="email" placeholder="Masukkan Email..." className="border
+              border-zinc-500 p-2 rounded-md" required/>
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="pesan" className="font-semibold">Pesan</label>
+              <textarea name="pesan" id="pesan" cols="45" rows="7" placeholder="Pesan..." className="border
+              border-zinc-500 p-2 rounded-md" required></textarea>
+            </div>
+            <div className="text-center">
+              <button className="bg-violet-700 p-3 rounded-lg w-full border
+              cursor-pointer border-zinc-600 hover:bg-violet-600" type="submit">Kirim Pesan</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      {/* Kontak */}
 
     </>
 
