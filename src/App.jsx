d314edde462e641d1c1189607e4a43bec170b6f9
muhammadsyauqijazuli <syauqialjm93@gmail.com>
2025-09-23 13:25:17 +0700

    modify Proyek Section

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <div className="hero grid md:grid-cols-2 pt-10 items-center xl:gap-0 gap-6 grid-cols-1">
         <div>
           <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
-            <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md" />
+            <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md" loading="lazy" />
             <q>kode yang indah, lahir dari ketekunan. 😁</q>
           </div>
           <h1 className="text-5xl/tight font-bold mb-6">Hi, Saya Syauqi Jazuli</h1>
@@ -29,14 +29,14 @@ function App() {
             </a>
           </div>
         </div>
-        <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto" />
+        <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto" loading="lazy" />
       </div>
 
 
       {/* tentang */}
       <div className="tentang mt-32 py-10">
         <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg">
-          <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md mb-10 sm:hidden" />
+          <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md mb-10 sm:hidden" loading="lazy"/>
           <p className="text-base/loose mb-10">
             I am currently a part of the Bangkit Academy cohort, where I am focusing on Cloud Computing
             . This program has sharpened my skills and prepared me to thrive, aiming to be a skilled and well-rounded
@@ -44,7 +44,7 @@ function App() {
           </p>
           <div className="flex items-center justify-between">
             <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md sm:block
-            hidden"/>
+            hidden" loading="lazy"/>
             <div className="flex items-center gap-6">
               <div>
                 <h1 className="text-4xl mb-1">
@@ -68,7 +68,7 @@ function App() {
             {listTools.map(tool => (
               <div className=" flex items-center gap-2 p-3 border border-zinc-600 group rounded-md
             hover:bg-zinc-800" key={tool.id}>
-                <img src={tool.gambar} alt="Tools Image" className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900" />
+                <img src={tool.gambar} alt="Tools Image" className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900" loading="lazy" />
                 <div>
                   <h4 className="font-bold">
                     {tool.nama}
@@ -92,19 +92,22 @@ function App() {
         <p className="text-base/loose text-center opacity-50">
             Berikut ini beberapa proyek yang sudah saya buat
         </p>
-        <div className="proyek-box mt-14 grid grid-cols-3 gap-4">
+        <div className="proyek-box mt-14 grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
             {listProyek.map(proyek => (
-              <div key={proyek.id}>
-                <img src={proyek.gambar} alt="Proyek Image"/>
+              <div key={proyek.id} className="p4 bg-zinc-800 rounded-md">
+                <img src={proyek.gambar} alt="Proyek Image" loading="lazy"/>
                 <div>
-                  <h1>{proyek.nama}</h1>
-                  <p>{proyek.desk}</p>
-                  <div>
+                  <h1 className="text-2xl font-bold my-4">{proyek.nama}</h1>
+                  <p className="text-base/loose mb-4">{proyek.desk}</p>
+                  <div className="flex flex-wrap gap-2">
                     {proyek.tools.map((tool, index) => (
-                      <p key={index}>{tool}</p>
+                      <p className="py-1 px-3 border border-zinc-500 bg-zinc-600 rounded-md font-semibold" key={index}>{tool}</p>
                     ))}
                   </div>
-                  <div></div>
+                  <div className="mt-8 text-center">
+                    <a href="#" className="bg-violet-700 p3 rounded-lg block border
+                    border-zinc-600 hover:bg-violet-600">Lihat Website</a>
+                  </div>
                 </div>
               </div>
             ))}
